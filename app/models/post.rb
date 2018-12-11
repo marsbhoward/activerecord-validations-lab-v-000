1dbb80f90a855b054a62d2eca6936d6fe5a7221a
Mars Howard <marsbhoward@gmail.com>
2018-12-11 00:50:05 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :is_clickbaity?
+
+  CLICKBAITY_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def is_clickbaity?
+    if CLICKBAITY_PATTERNS.none? { |pattern| pattern.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
